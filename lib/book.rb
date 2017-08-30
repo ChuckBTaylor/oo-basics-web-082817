f79b1300bf11ccefc932f7ff358b8cd8ea461d86
ChuckBTaylor <WLScripps@gmail.com>
2017-08-30 13:53:20 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
 
+  def initialize(t)
+    @title = t
+  end
 
+  attr_accessor :title, :author, :page_count, :genre
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+class Shoe
+
+  def initialize(b)
+    @brand = b
+  end
+
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+
+end
